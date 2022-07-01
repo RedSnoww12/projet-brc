f3d3e400970fb338d17342110e788e9976e0aaaf
djimta <67872414+djimta@users.noreply.github.com>
2022-07-01 19:37:54 +0200

    Catalogue de paire done
    ShopSection fini, manque a ajouter la bdd des event et les info

--- client/src/components/ShopSection/ShopSection.jsx
+++ client/src/components/ShopSection/ShopSection.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../ShopSection/ShopSection.css'
 import axios from "axios";
 import { useEffect } from 'react';
-
+import ArtcImg from '../../assets/nike-sb-dunk-low-what-the-p-rod.jpg';
 
 const ShopSection = () => {
 
@@ -10,7 +10,7 @@ const ShopSection = () => {
 
     useEffect(() => {
         const fetchShoes = async () => {
-           const { data } = await axios.get("/shoes");
+           const { data } = await axios.get("/api/shoes");
             setShoes(data); 
         }
         fetchShoes();
@@ -25,27 +25,38 @@ const ShopSection = () => {
                 <button>A venir</button>
             </ul>
         </div>
-        <div className="ArticleView">
-            {shoes.map((shoe) => (
-                <div className='' key={shoe._id}>
-                    <div>
-                        <img src={shoe.shoes_image} alt={shoe.shoes_name} />
+        <div className="ArticleView-GridContainer">
+                
+            {shoes.map((shoe) => (         
+                    <div className="ArticleCard" key={shoe._id}> 
+                    <div className="card-header">
+                        <time dateTime='2202-07-02'>
+                            <span className="h4">05</span>
+                            <span className="h5 small">Jul</span>
+                        </time>
                     </div>
+                            <div className="photo-product">
+                                    <div>
+                                        <img src={ArtcImg} />
+                                    </div>
+                            </div>
+                            <div className="desc-product">
+                                <h2>
+                                        {shoe.shoes_name}
+                                </h2>
+                            </div>
+                            <div className="buying-product">
+                                <div className="price">
+                                    <p>Prix de Base : {shoe.starting_price}$🔥 </p>
+                                </div>
+                                <div className="buying-btn">
 
-                    <p>
-                        {shoe.shoes_name}
-                    </p>
+                                </div>
+                            </div>                
 
-                    <p>
-                        {shoe.description}
-                    </p>
-
-                    <p>
-                        {shoe.starting_price}
-                    </p>
-
-                </div>
-            ))}
+                    </div>
+            ))} 
+            
         </div>
     </div>
     
